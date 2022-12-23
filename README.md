ca55db7a79bfc4d4ae5de2fe3b95ecc5093b7639
akash <aprajput331998@gmail.com>
2022-12-23 17:25:39 +0530

    Third commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 First Nodejs demo for Github.
 Adding some text in the second commit.
+Adding some text for third commit.
---
+++ modules.js
@@ -0,0 +1,53 @@
+// const add = (a, b) => {
+//     return a + b;
+// }
+
+// module.exports = add;
+
+
+const add = (a, b) => {
+    return a + b;
+}
+const sub = (a, b) => {
+    return a - b;
+}
+const multiply = (a, b) => {
+    return a * b;
+}
+const divide = (a, b) => {
+    return a / b;
+}
+
+const name = "Akash";
+
+const car = {
+    brand: "Tata",
+    model: "Altroz",
+    color: "Black"
+};
+
+const marvel = [
+    Doctor_Doom = {
+        category: "Supervillian",
+        Team_affiliation: "Dark Avengers",
+        wears_a_cape: true
+    },
+    Venom = {
+        category: "Anti-hero",
+        Team_affiliation: "Dark Avengers",
+        wears_a_cape: false
+    },
+    Captain_America = {
+        category: "Superhero",
+        Team_affiliation: "Avengers",
+        wears_a_cape: false
+    },
+    Infamous_Ironman = {
+        category: "Superhero",
+        Team_affiliation: "Avengers",
+        wears_a_cape: true
+    },
+
+];
+
+module.exports = {add, sub, multiply, divide, name, car, marvel}
